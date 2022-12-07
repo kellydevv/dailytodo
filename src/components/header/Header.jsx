bf47f3c1d6694dde7e875021efc7287f54cd48ac
정현진 <jhj9328@hanmail.net>
2022-12-07 17:59:38 +0900

    [create]new project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header () {
+      return(
+            
+      )
+}
+
+export default Header;
+
+const StHeader = styled.div`
+width:1200px;
+`;
